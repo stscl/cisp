9779c7bf7c132e4112c853f1a96fba9583fec63c
SpatLyu <lyu.geosocial@gmail.com>
2024-11-13 21:25:14 +0800

    add spc docs

--- R/spc.R
+++ R/spc.R
@@ -1,3 +1,29 @@
+#' spatial pattern correlation
+#'
+#' @param data A `data.frame`, `tibble` or `sf` object of observation data.
+#' @param overlay (optional) Spatial overlay method. One of `and`, `or`, `intersection`.
+#' Default is `and`.
+#' @param discnum A numeric vector of discretized classes of columns that need to be discretized.
+#' Default all `discvar` use `3:8`.
+#' @param minsize (optional) The min size of each discretization group. Default all use `1`.
+#' @param strategy (optional) Optimal discretization strategy. When `strategy` is `1L`, choose the highest
+#' q-statistics to determinate optimal spatial data discretization parameters. When `strategy` is `2L`,
+#' The optimal discrete parameters of spatial data are selected by combining LOESS model.
+#' @param increase_rate (optional) The critical increase rate of the number of discretization. Default is `5%`.
+#' @param cores (optional) Positive integer (default is 1). When cores are greater than 1, use
+#' multi-core parallel computing.
+#'
+#' @return A list.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' ## The following code needs to configure the Python environment to run:
+#' sim1 = gdverse::sim
+#' g = spc(sim1, discnum = 3:6, cores = 1)
+#' g
+#' }
+#'
 spc = \(data, overlay = 'and', discnum = 3:8, minsize = 1,
         strategy = 2L, increase_rate = 0.05, cores = 1){
   if (inherits(data,'sf')) {
@@ -16,5 +42,6 @@ spc = \(data, overlay = 'and', discnum = 3:8, minsize = 1,
   res = purrr::map(xsname,calcul_spcv,overlay = overlay,discnum = discnum,
                    minsize = minsize, strategy = strategy,
                    increase_rate = increase_rate, cores = cores)
+  names(res) = xsname
   return(res)
 }
