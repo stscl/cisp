3b42bbf9739120eab25a353ef6c098b1487956d1
SpatLyu <lyu.geosocial@gmail.com>
2024-11-13 12:10:57 +0800

    finish ssh_marginalcontri

--- R/ssh_marginalcontri.R
+++ R/ssh_marginalcontri.R
@@ -1,3 +1,25 @@
+#' spatial association marginal contributions derived from spatial stratified heterogeneity
+#'
+#' @param formula A formula of ISP model.
+#' @param data A `data.frame`, `tibble` or `sf` object of observation data.
+#' @param overlay (optional) Spatial overlay method. One of `and`, `or`, `intersection`.
+#' Default is `and`.
+#' @param cores (optional) Positive integer (default is 1). When cores are greater than 1, use
+#' multi-core parallel computing.
+#'
+#' @return A list.
+#' \describe{
+#' \item{\code{pd}}{robust power of determinants}
+#' \item{\code{spd}}{shap power of determinants}
+#' \item{\code{determination}}{determination of the optimal interaction of variables}
+#' }
+#' @export
+#'
+#' @examples
+#' NTDs = sf::st_as_sf(gdverse::NTDs, coords = c('X','Y'))
+#' g = sesp(incidence ~ ., data = NTDs, cores = 1)
+#' g
+#'
 ssh_marginalcontri = \(formula, data, overlay = 'and', cores = 1){
   formula = stats::as.formula(formula)
   formula.vars = all.vars(formula)
@@ -13,48 +35,11 @@ ssh_marginalcontri = \(formula, data, overlay = 'and', cores = 1){
 
   yname = formula.vars[1]
   xname = colnames(data)[-which(colnames(data) == yname)]
-  if (is.null(discvar)) {
-    xdiscname = xname
-    xundiscname = NULL
-  } else {
-    xdiscname = discvar
-    xundiscname = xname[-which(xname %in% discvar)]
-  }
-  discdf = dplyr::select(data,dplyr::all_of(c(yname,xdiscname)))
-  rgd_res = gdverse::rgd(paste0(yname," ~ ."), data = discdf,
-                         discnum = discnum, minsize = minsize, cores = cores)
-  qs = rgd_res[[1]]
-  dti = rgd_res[[2]]
-  qs$variable = factor(qs$variable,levels = xdiscname)
-  qs = dplyr::rename(qs,qvalue = `Q-statistic`)
-
-  if (strategy == 1L) {
-    opt_discnum = dplyr::group_split(qs,variable) |>
-      purrr::map_dbl(\(.df) .df$discnum[which.max(.df$qvalue)])
-  } else {
-    suppressWarnings({opt_discnum = dplyr::group_split(qs,variable) |>
-      purrr::map_dbl(\(.df) sdsfun::loess_optnum(.df$qvalue, .df$discnum,
-                                                 increase_rate = increase_rate)[1])})
-  }
-  res_discdf = purrr::map_dfc(seq_along(opt_discnum),
-                              \(.n) {dn = which(dti$discnum == opt_discnum[.n])
-                              return(dti[dn,.n])})
-
-  if (!is.null(xundiscname)){
-    dti = data %>%
-      dplyr::select(dplyr::any_of(c(yname,xundiscname))) %>%
-      dplyr::bind_cols(res_discdf)
-  } else {
-    dti = data %>%
-      dplyr::select(dplyr::any_of(yname)) %>%
-      dplyr::bind_cols(res_discdf)
-  }
-
-  xname = colnames(dti)[-which(colnames(dti) == yname)]
-  xs = generate_subsets(xname,empty = FALSE, self = TRUE)
+  dti = dplyr::select(data,,dplyr::all_of(xname))
+  xs = sdsfun::generate_subsets(xname,empty = FALSE, self = TRUE)
   spfom = overlay
 
-  rpd_mc = \(formula, discdata, overlaymethod = 'and'){
+  pd_mc = \(formula, discdata, overlaymethod = 'and'){
     formula = stats::as.formula(formula)
     formula.vars = all.vars(formula)
     if (formula.vars[2] != "."){
@@ -62,8 +47,8 @@ ssh_marginalcontri = \(formula, data, overlay = 'and', cores = 1){
     }
     yname = formula.vars[1]
     if (overlaymethod == 'intersection'){
-      fuzzyzone = discdata %>%
-        dplyr::select(-dplyr::any_of(yname)) %>%
+      fuzzyzone = discdata |>
+        dplyr::select(-dplyr::any_of(yname)) |>
         purrr::reduce(paste,sep = '_')
     } else {
       fuzzyzone = sdsfun::fuzzyoverlay(formula,discdata,overlaymethod)
@@ -72,8 +57,9 @@ ssh_marginalcontri = \(formula, data, overlay = 'and', cores = 1){
     return(qtheta)
   }
 
-  calcul_rpd = \(.x){
-    qv = rpd_isp(paste(yname,'~',paste0(.x,collapse = '+')),dti,spfom)
+  calcul_pd = \(.x){
+    qv = pd_mc(paste(yname,'~',paste0(.x,collapse = '+')),dti,spfom)
+    names(qv) = "pd"
     return(qv)
   }
 
@@ -85,13 +71,13 @@ ssh_marginalcontri = \(formula, data, overlay = 'and', cores = 1){
   }
 
   if (doclust) {
-    parallel::clusterExport(cores,c('factor_detector'))
-    out_rpd = parallel::parLapply(cores, xs, calcul_rpd)
-    out_rpd = tibble::as_tibble(do.call(rbind, out_rpd))
+    parallel::clusterExport(cores)
+    out_pdv = parallel::parLapply(cores, xs, calcul_pd)
+    out_pdv = tibble::as_tibble(do.call(rbind, calcul_pd))
   } else {
-    out_rpd = purrr::map_dfr(xs, calcul_rpd)
+    out_pdv = purrr::map_dfr(xs, calcul_pd)
   }
-  out_rpdv = dplyr::pull(out_rpd,1)
+  out_pdv = dplyr::pull(out_pdv,1)
   m = length(xname)
   mf = factorial(m)
 
@@ -109,7 +95,7 @@ ssh_marginalcontri = \(formula, data, overlay = 'and', cores = 1){
 
   calcul_shap = \(xvar){
     fullxvar = xname[-which(xname == xvar)]
-    fullvar = generate_subsets(fullxvar,empty = FALSE,self = TRUE)
+    fullvar = sdsfun::generate_subsets(fullxvar,empty = FALSE,self = TRUE)
 
     calcul_unishap = \(xvar,fullxvar,namelist,valuevec){
       n = length(fullxvar)
@@ -119,14 +105,14 @@ ssh_marginalcontri = \(formula, data, overlay = 'and', cores = 1){
       return(thetax)
     }
 
-    thetaxs = purrr::map_dbl(fullvar, \(.x) calcul_unishap(xvar,.x,xs,out_rpdv))
+    thetaxs = purrr::map_dbl(fullvar, \(.x) calcul_unishap(xvar,.x,xs,out_pdv))
     thetax = sum(thetaxs)
-    names(thetax) = 'SPD'
+    names(thetax) = 'spd'
     return(thetax)
   }
 
   if (doclust) {
-    parallel::clusterExport(cores,c('generate_subsets'))
+    parallel::clusterExport(cores)
     out_g = parallel::parLapply(cores,xname,calcul_shap)
     out_g = tibble::as_tibble(do.call(rbind, out_g))
   } else {
@@ -136,28 +122,17 @@ ssh_marginalcontri = \(formula, data, overlay = 'and', cores = 1){
 
   IntersectionSymbol = rawToChar(as.raw(c(0x20, 0xE2, 0x88, 0xA9, 0x20)))
   xsname = purrr::map_chr(xs,\(.x) paste(.x,collapse = IntersectionSymbol))
-  interactvar = xs[[which.max(out_rpdv)]]
-  if (overlay == 'intersection'){
-    reszone = dti %>%
-      dplyr::select(dplyr::all_of(interactvar)) %>%
-      purrr::reduce(paste,sep = '_')
-  } else {
-    reszone = sdsfun::fuzzyoverlay(paste(yname,'~',paste0(interactvar,collapse = '+')),
-                                   dti, overlay)
-  }
-  zonenum = as.numeric(table(reszone))
-  percentzone = length(which(zonenum==1)) / length(reszone)
-  risk1 = risk_detector(dti[,yname,drop = TRUE],reszone,alpha)
-  res_rpd = tibble::tibble(variable = xsname) %>%
-    dplyr::bind_cols(out_rpd) %>%
-    dplyr::arrange(dplyr::desc(rpd))
+  interactvar = xs[[which.max(out_pdv)]]
+  res_pd = tibble::tibble(variable = xsname,
+                          pd = out_pdv) |>
+    dplyr::arrange(dplyr::desc(pd))
   res_spd = tibble::tibble(variable = xname,
-                           spd = out_spd) %>%
+                           spd = out_spd) |>
     dplyr::arrange(dplyr::desc(spd))
 
   step_interaction = sapply(xs, length)
   max_rpd_names = sapply(unique(step_interaction), function(.s) {
-    step_rpd = out_rpdv[which(step_interaction == .s)]
+    step_rpd = out_pdv[which(step_interaction == .s)]
     step_indice = which.max(step_rpd)
     return(xs[which(step_interaction == .s)][step_indice])
   })
@@ -178,18 +153,15 @@ ssh_marginalcontri = \(formula, data, overlay = 'and', cores = 1){
     }
   }
   determination = tibble::tibble(variable = xsname[new_rpd_indice],
-                                 rpd = out_rpdv[new_rpd_indice],
+                                 rpd = out_pdv[new_rpd_indice],
                                  step = step_interaction[new_rpd_indice],
-                                 name = new_xsname[nchar(new_xsname) > 0]) %>%
-    dplyr::group_by(step) %>%
-    dplyr::arrange(rpd,.by_group=TRUE) %>%
+                                 name = new_xsname[nchar(new_xsname) > 0]) |>
+    dplyr::group_by(step) |>
+    dplyr::arrange(pd,.by_group=TRUE) |>
     dplyr::ungroup()
 
-  res = list("rpd" = res_rpd, "spd" = res_spd, "optdisc" = res_discdf,
-             "risk" = risk1, "determination" = determination,
-             "number_individual_explanatory_variables" = length(interactvar),
-             "number_overlay_zones" = length(zonenum),
-             "percentage_finely_divided_zones" =  percentzone)
-  class(res) = "cdsp_result"
+  res = list("spd" = res_spd, "pd" = res_pd,
+             "determination" = determination)
+  class(res) = "sshmc_result"
   return(res)
 }
